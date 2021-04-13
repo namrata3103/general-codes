4cda5a34b57d340e0179a22c8fc13beeea9740e6
Namrata Sharma <82275938+namrata3103@users.noreply.github.com>
2021-04-14 00:46:59 +0530

    Update Factorial.cpp

--- Factorial.cpp
+++ Factorial.cpp
@@ -3,10 +3,11 @@ using namespace std;
 int main()
 {
 	double i, n, c = 1;
+	cout<<"Enter any number: ";
 	cin >> n;
 	for (i = 1; i <= n; i++)
 	{
 		c = c * i;
 	}
-	cout << c;
+	cout << Factorial is: "<< c;
 }
